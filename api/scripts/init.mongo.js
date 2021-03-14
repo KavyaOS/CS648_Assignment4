94cd25e3b86a0ccf5e19a44ab998e052c7de0f3c
KavyaOS <kavya.os15@gmail.com>
2021-03-13 23:10:17 -0800

    code modularized

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -1,3 +1,6 @@
+/* eslint linebreak-style: ["error","windows"] */
+/* global db print */
+/* eslint no-restricted-globals: "off" */
 db.products.remove({});
 
 const productsDB = [];
@@ -6,8 +9,8 @@ db.products.insertMany(productsDB);
 const count = db.products.count();
 print('Inserted', count, 'products');
 
-db.counters.remove({ _id: 'products'})
-db.counters.insert({ _id: 'products', current: count})
+db.counters.remove({ _id: 'products' });
+db.counters.insert({ _id: 'products', current: count });
 
 db.products.createIndex({ id: 1 }, { unique: true });
 db.products.createIndex({ category: 1 });
